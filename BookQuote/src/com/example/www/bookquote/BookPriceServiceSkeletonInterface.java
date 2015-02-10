e004590be4c28a4b536fc57d14f3c3fb887caeb2
Floris Smit <florissmit10@gmail.com>
2015-02-10 11:09:38 +0100

    Added example project of the tutorial

---
+++ BookQuote/src/com/example/www/bookquote/BookPriceServiceSkeletonInterface.java
@@ -0,0 +1,29 @@
+
+/**
+ * BookPriceServiceSkeletonInterface.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.2  Built on : Apr 17, 2012 (05:33:49 IST)
+ */
+    package com.example.www.bookquote;
+    /**
+     *  BookPriceServiceSkeletonInterface java skeleton interface for the axisService
+     */
+    public interface BookPriceServiceSkeletonInterface {
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                    * @param getBookPrice
+         */
+
+        
+                public com.example.www.bookquote.GetBookPriceResponse getBookPrice
+                (
+                  com.example.www.bookquote.GetBookPrice getBookPrice
+                 )
+            ;
+        
+         }
+    
